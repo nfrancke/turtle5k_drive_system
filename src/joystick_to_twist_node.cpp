e5608b002443d4c635a2de8ca43f5fef3f329f07
Niek <niek@niek-M740TU-M760TU.(none)>
2015-12-01 11:57:14 +0100

    changed topic names and build a safety in it
     Committer: Niek <niek@niek-M740TU-M760TU.(none)>
    	modified:   joystick_twist_test.launch
    	modified:   joystick_twist_test.launch~
    	modified:   src/joystick_to_twist_node.cpp

--- src/joystick_to_twist_node.cpp
+++ src/joystick_to_twist_node.cpp
@@ -1,42 +1,116 @@
+/*****************************************************************************************************************************************
+Function: 
+This program converts the joystick message from joynode to a twist message for a turtle 5k.
+
+Pre:
+
+
+Post:
+vector that gives x and y direction in m/s. and a rotation around de z-ax in rad/s
+
+Writer 		: Vincent Bevaart
+date 		: 1-12-2015
+********************************************************************************************************************************************/
 // includes
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
 
 #define ROS_FRAME_RATE 100
+#define SUB_TOPIC_NAME 					"joy"
+#define SUB_TOPIC_BUFFER_SIZE			1
+#define PUB_TOPIC_NAME 					"mcWheelVelocityMps"
+#define PUB_TOPIC_BUFFER_SIZE			1
 
-//create class motordrive, functions and variables
-class MotorDriver123
+
+/*****************************************************************************************************************************************
+Start defining class Subscribe
+********************************************************************************************************************************************/
+class Subscriber
 {
 public:
-  double dVelAxisX;
-  double dVelAxisY;
-  double dVelAxisZ;
+	//axis values
+	double dVelAxisX;
+	double dVelAxisY;
+	double dVelAxisZ;
 
-  MotorDriver123();
-  void sendValue();
+///////////////////////////////////////////////////////////////////////////////////////////
+//Function: makes publisher and subscriber and read the params for this node.
+//pre: 	-
+//post: 
+///////////////////////////////////////////////////////////////////////////////////////////
+	Subscriber();
+	void sendValue();
 
 private:
-  void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
+	///////////////////////////////////////////////////////////////////////////////////////////
+	//Function: read joystick message.
+	//pre: 	
+	//post: 
+	/////////////////////////////////////////////////////////////////////////////////////////// 
+	void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
   
-  ros::NodeHandle nh_;
 
-  int iAxisX, iAxisY, iAxisZ;
-  double dScaleAxisX, dScaleAxisY, dScaleAxisZ;
-
-  ros::Publisher pub_VelPub;
-  ros::Subscriber sub_VelSub;
-  geometry_msgs::Twist msg;
+	int iAxisX, iAxisY, iAxisZ;			//define joystick button for linear x, y and angular z
+	double dScaleAxisX, dScaleAxisY, dScaleAxisZ;	
+	ros::NodeHandle nh_;	
+	ros::Publisher pub_VelPub;
+	ros::Subscriber sub_VelSub;
+	geometry_msgs::Twist msg;
   
 };
+/*****************************************************************************************************************************************
+end of defining class Subscribe
+********************************************************************************************************************************************/
 
-// receive output Joystick from parameter file and set parameters
-MotorDriver123::MotorDriver123(){
+/*****************************************************************************************************************************************
+Start of main
+********************************************************************************************************************************************/
+int main(int argc, char** argv){
+	
+	//init
+	ros::init(argc, argv, "joystick_to_twist_node");
+	ROS_INFO_ONCE("ROS is initialized");
+  	Subscriber sub;
+
+  	//set frame rate for ROS while loop
+	ros::Rate loop_rate(ROS_FRAME_RATE);
+
+
+	while(ros::ok())
+	{
+		sub.sendValue();
+		ROS_INFO_ONCE("first value is send");
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+	return 0;
+}
+
+/*****************************************************************************************************************************************
+End of main
+********************************************************************************************************************************************/
+
+/*****************************************************************************************************************************************
+Functions
+********************************************************************************************************************************************/
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//Function: makes publisher and subscriber and read the params for this node.
+//pre: 	-
+//post: 
+///////////////////////////////////////////////////////////////////////////////////////////
+Subscriber::Subscriber(){
 
 	// Read joy topic
-	  sub_VelSub = nh_.subscribe<sensor_msgs::Joy>("joy", 1, &MotorDriver123::joyCallback, this);
+	  sub_VelSub = nh_.subscribe<sensor_msgs::Joy>(SUB_TOPIC_NAME, SUB_TOPIC_BUFFER_SIZE, &Subscriber::joyCallback, this);
 	// Publish Twist message with motorspeed
-	  pub_VelPub = nh_.advertise<geometry_msgs::Twist>("mcWheelVelocityTwist", 1);
+	  pub_VelPub = nh_.advertise<geometry_msgs::Twist>(PUB_TOPIC_NAME, PUB_TOPIC_BUFFER_SIZE);
+
+	  //define values for safety
+	  dScaleAxisX = 0;
+	  dScaleAxisY = 0;
+	  dScaleAxisZ = 0;
 
 	//Check params x-axis, y-axis, z-axis
 		if (nh_.hasParam("iAxisX"),nh_.hasParam("iAxisY"), nh_.hasParam("iAxisZ"))
@@ -66,16 +140,26 @@ MotorDriver123::MotorDriver123(){
 			dScaleAxisX=0; dScaleAxisY=0, dScaleAxisZ=0;
 		}
 }
-// set speed values and put them into a vector
-void MotorDriver123::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//Function: read joystick message.
+//pre: 	
+//post: 
+/////////////////////////////////////////////////////////////////////////////////////////// 
+void Subscriber::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
-	// calculate speedvalues
+	// read and calculate speedvalues
 	dVelAxisX = dScaleAxisX*joy->axes[iAxisX];
 	dVelAxisY = dScaleAxisY*joy->axes[iAxisY];
 	dVelAxisZ = dScaleAxisZ*joy->axes[iAxisZ];
 }
 
-void MotorDriver123::sendValue(){
+///////////////////////////////////////////////////////////////////////////////////////////
+//Function: send twist value on ROS.
+//pre: 	
+//post: 
+/////////////////////////////////////////////////////////////////////////////////////////// 
+void Subscriber::sendValue(){
 	//put speedvalues into vector
 
 	msg.linear.x = dVelAxisX;
@@ -87,23 +171,4 @@ void MotorDriver123::sendValue(){
 	ROS_DEBUG("Actual motor set state: Motor1: %f, Motor2: %f, Motor3: %f.", dVelAxisX, dVelAxisY, dVelAxisZ);
 	ROS_DEBUG("Actual scaler values: Scaler1: %f, Scaler2: %f, Scaler3: %f.", dScaleAxisX, dScaleAxisY, dScaleAxisZ);
 }
-
-// Main
-int main(int argc, char** argv){
-	
-	ros::init(argc, argv, "MotorDrivers");
-	ROS_INFO_ONCE("ROS is initialized");
-  	MotorDriver123 MotorDrivers;
-	ros::Rate loop_rate(ROS_FRAME_RATE);
-
-	while(ros::ok())
-	{
-		MotorDrivers.sendValue();
-		ROS_INFO_ONCE("first value is send");
-		ros::spinOnce();
-		loop_rate.sleep();
-	}
-	return 0;
-}
-
 
